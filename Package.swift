d0b4ed6751b2af09b39523624e9f137acc02b798
ahmadmau <ahmad.maulana@mekari.com>
2022-04-06 21:43:37 +0700

    Create shell commands

--- Package.swift
+++ Package.swift
@@ -3,26 +3,26 @@
 
 import PackageDescription
 
+let version = "1.0.0"
+let moduleName = "AMNetworking"
+let checksum = "8f42f67a0665798e1c4e149c73a36fb9dddc062bc90e3b4e077ea571613ddda1"
+
 let package = Package(
-    name: "AMNetworkingRemote",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "AMNetworkingRemote",
-            targets: ["AMNetworkingRemote"]),
+    name: moduleName,
+    platforms: [
+        .iOS(.v13) //, .macCatalyst(.v15), .macOS(.v10_15)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .library(
+            name: moduleName,
+            targets: [moduleName]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "AMNetworkingRemote",
-            dependencies: []),
-        .testTarget(
-            name: "AMNetworkingRemoteTests",
-            dependencies: ["AMNetworkingRemote"]),
+        .binaryTarget(
+            name: moduleName,
+            url: "https://github.com/ahmdmau/AMNetworkingRemote/releases/download/\(version)/\(moduleName).xcframework.zip",
+            checksum: checksum
+        )
     ]
 )
